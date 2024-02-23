076b02dfeca2b823eadb6455d9531a1e6cabe0f7
NikolasLucansky <nikolas.lucansky@gmail.com>
2024-02-23 11:59:37 +0100

    Init

---
+++ functions/resource_group.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from cdktf import TerraformResourceLifecycle
+# Resource Group Logic
+from cdktf_cdktf_provider_azurerm.resource_group import ResourceGroup
+from cdktf_cdktf_provider_azurerm.data_azurerm_resource_group import DataAzurermResourceGroup
+
+
+def generate_resource_tags(_tags):
+    tags = {}
+    if _tags is None:
+        return None
+    for key, value in _tags.items():
+        tags[key] = value
+    return tags
+
+
+def create_resource_group(_self, _environment, _location, _name, _tags=None):
+    return ResourceGroup(
+        _self,
+        "rg-{}-{}-{}".format(
+            _environment,
+            _location,
+            _name
+        ),
+        name="rg-{}-{}-{}".format(
+            _environment,
+            _location,
+            _name
+        ),
+        location=_location,
+        tags=_tags,
+        lifecycle=TerraformResourceLifecycle(
+            ignore_changes=['tags']
+        )
+    )
+
+
+def get_resource_group(_self, _resource, _resource_group_name):
+    return DataAzurermResourceGroup(
+        _self,
+        "data-{}-{}".format(_resource, _resource_group_name),
+        name=_resource_group_name
+    )
